b6ec6a55ff697cb99cc090caf5475ebf42752e28
ManavGhaiCode <manavghai12october@gmail.com>
2023-09-03 18:00:00 +0530

    add Gates folder/clear main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,1 @@
 #include <iostream>
-
-#include "./BaseGates.h"
-#include "Gates.h"
-
-int main() {
-    Gates::XORGate xor0;
-
-    xor0.input[0] = 0;
-    xor0.input[1] = 0;
-
-    xor0.run();
-
-    std::cout << xor0.output << std::endl;
-
-    return 0;
-}

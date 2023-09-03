31a0d2edab0abae4eea9e6016f79558f3b205c76
ManavGhaiCode <manavghai12october@gmail.com>
2023-09-03 14:36:26 +0530

    Separate nand gate

--- main.cpp
+++ main.cpp
@@ -1,27 +1,10 @@
 #include <iostream>
 
 #include "./BaseGates.h"
-
-class nandGate : public Gates::Gate {
-    private:
-        Gates::notGate not0;
-        Gates::andGate and0;
-
-    public:
-        void run() override {
-            and0.input[0] = input[0];
-            and0.input[1] = input[1];
-            and0.run();
-
-            not0.input = and0.output;
-            not0.run();
-
-            output = not0.output;
-        }
-};
+#include "Gates.h"
 
 int main() {
-    nandGate nand;
+    Gates::nandGate nand;
 
     nand.input[0] = 0;
     nand.input[1] = 1;

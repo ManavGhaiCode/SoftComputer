88dae800d19bd47a64d9ade78aa47223805cfbcf
ManavGhaiCode <manavghai12october@gmail.com>
2023-09-03 19:36:16 +0530

    add Adder8Bit

--- Chips/Chips.h
+++ Chips/Chips.h
@@ -11,7 +11,7 @@ namespace Chips {
             Gates::orGate or0;
 
         public:
-            void run() {
+            void run() override {
                 xor0.input[0] = input[0];
                 xor0.input[1] = input[1];
                 xor0.run();
@@ -36,6 +36,80 @@ namespace Chips {
 
                 output[0] = xor1.output;
                 output[1] = or0.output;
+            }
+    };
+
+    class Adder8Bit : public Gates::CustomGate<16, 9> {
+        private:
+            Adder adder0;
+            Adder adder1;
+            Adder adder2;
+            Adder adder3;
+            Adder adder4;
+            Adder adder5;
+            Adder adder6;
+            Adder adder7;
+        
+        public:
+            void run() {
+                adder0.input[0] = input[0];
+                adder0.input[1] = input[8];
+                adder0.input[2] = 0;
+
+                adder0.run();
+
+                adder1.input[0] = input[1];
+                adder1.input[1] = input[9];
+                adder1.input[2] = adder0.output[1];
+
+                adder1.run();
+
+                adder2.input[0] = input[2];
+                adder2.input[1] = input[10];
+                adder2.input[2] = adder1.output[1];
+
+                adder2.run();
+
+                adder3.input[0] = input[3];
+                adder3.input[1] = input[11];
+                adder3.input[2] = adder2.output[1];
+
+                adder3.run();
+
+                adder4.input[0] = input[4];
+                adder4.input[1] = input[12];
+                adder4.input[2] = adder3.output[1];
+
+                adder4.run();
+
+                adder5.input[0] = input[5];
+                adder5.input[1] = input[13];
+                adder5.input[2] = adder4.output[1];
+
+                adder5.run();
+
+                adder6.input[0] = input[6];
+                adder6.input[1] = input[14];
+                adder6.input[2] = adder5.output[1];
+
+                adder6.run();
+
+                adder7.input[0] = input[7];
+                adder7.input[1] = input[15];
+                adder7.input[2] = adder6.output[1];
+
+                adder7.run();
+
+                output[0] = adder0.output[0];
+                output[1] = adder1.output[0];
+                output[2] = adder2.output[0];
+                output[3] = adder3.output[0];
+                output[4] = adder4.output[0];
+                output[5] = adder5.output[0];
+                output[6] = adder6.output[0];
+                output[7] = adder7.output[0];
+
+                output[8] = adder7.output[1];
             }
     };
 }
--- main.cpp
+++ main.cpp
@@ -3,16 +3,39 @@
 #include "./Chips/Chips.h"
 
 int main() {
-    Chips::Adder adder0;
+    Chips::Adder8Bit adder0;
 
     adder0.input[0] = 1;
-    adder0.input[1] = 1;
+    adder0.input[1] = 0;
     adder0.input[2] = 1;
+    adder0.input[3] = 0;
+    adder0.input[4] = 0;
+    adder0.input[5] = 0;
+    adder0.input[6] = 0;
+    adder0.input[7] = 0;
+
+    adder0.input[8] = 1;
+    adder0.input[9] = 1;
+    adder0.input[10] = 1;
+    adder0.input[11] = 1;
+    adder0.input[12] = 0;
+    adder0.input[13] = 0;
+    adder0.input[14] = 0;
+    adder0.input[15] = 0;
+
 
     adder0.run();
 
-    std::cout << adder0.output[0] << std::endl;
+    std::cout << adder0.output[7] << std::endl;
+    std::cout << adder0.output[6] << std::endl;
+    std::cout << adder0.output[5] << std::endl;
+    std::cout << adder0.output[4] << std::endl;
+    std::cout << adder0.output[3] << std::endl;
+    std::cout << adder0.output[2] << std::endl;
     std::cout << adder0.output[1] << std::endl;
+    std::cout << adder0.output[0] << std::endl;
+
+    std::cout << std::endl << adder0.output[8] << std::endl;
 
     return 0;
 }

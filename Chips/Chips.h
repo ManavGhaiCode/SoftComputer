6df1b5a5b4df85a33d9a02878d2656163a93e541
ManavGhaiCode <manavghai12october@gmail.com>
2023-09-04 14:08:46 +0530

    add Adder16Bit

--- Chips/Chips.h
+++ Chips/Chips.h
@@ -111,5 +111,144 @@ namespace Chips {
 
                 output[8] = adder7.output[1];
             }
+
+    };
+
+    class Adder16Bit : public Gates::CustomGate<16, 9> {
+        private:
+            Adder adder0;
+            Adder adder1;
+            Adder adder2;
+            Adder adder3;
+            Adder adder4;
+            Adder adder5;
+            Adder adder6;
+            Adder adder7;
+            Adder adder8;
+            Adder adder9;
+            Adder adder10;
+            Adder adder11;
+            Adder adder12;
+            Adder adder13;
+            Adder adder14;
+            Adder adder15;
+        
+        public:
+            void run() {
+                adder0.input[0] = input[0];
+                adder0.input[1] = input[16];
+                adder0.input[2] = 0;
+
+                adder0.run();
+
+                adder1.input[0] = input[1];
+                adder1.input[1] = input[17];
+                adder1.input[2] = adder0.output[1];
+
+                adder1.run();
+
+                adder2.input[0] = input[2];
+                adder2.input[1] = input[18];
+                adder2.input[2] = adder1.output[1];
+
+                adder2.run();
+
+                adder3.input[0] = input[3];
+                adder3.input[1] = input[19];
+                adder3.input[2] = adder2.output[1];
+
+                adder3.run();
+
+                adder4.input[0] = input[4];
+                adder4.input[1] = input[20];
+                adder4.input[2] = adder3.output[1];
+
+                adder4.run();
+
+                adder5.input[0] = input[5];
+                adder5.input[1] = input[21];
+                adder5.input[2] = adder4.output[1];
+
+                adder5.run();
+
+                adder6.input[0] = input[6];
+                adder6.input[1] = input[22];
+                adder6.input[2] = adder5.output[1];
+
+                adder6.run();
+
+                adder7.input[0] = input[7];
+                adder7.input[1] = input[23];
+                adder7.input[2] = adder6.output[1];
+
+                adder7.run();
+
+                adder8.input[0] = input[8];
+                adder8.input[1] = input[24];
+                adder8.input[2] = adder7.output[1];
+
+                adder8.run();
+
+                adder9.input[0] = input[9];
+                adder9.input[1] = input[25];
+                adder9.input[2] = adder8.output[1];
+
+                adder9.run();
+
+                adder10.input[0] = input[10];
+                adder10.input[1] = input[26];
+                adder10.input[2] = adder9.output[1];
+
+                adder10.run();
+
+                adder11.input[0] = input[11];
+                adder11.input[1] = input[27];
+                adder11.input[2] = adder10.output[1];
+
+                adder11.run();
+
+                adder12.input[0] = input[12];
+                adder12.input[1] = input[28];
+                adder12.input[2] = adder11.output[1];
+
+                adder12.run();
+
+                adder13.input[0] = input[13];
+                adder13.input[1] = input[29];
+                adder13.input[2] = adder12.output[1];
+
+                adder13.run();
+
+                adder14.input[0] = input[14];
+                adder14.input[1] = input[30];
+                adder14.input[2] = adder13.output[1];
+
+                adder14.run();
+
+                adder15.input[0] = input[15];
+                adder15.input[1] = input[31];
+                adder15.input[2] = adder14.output[1];
+
+                adder15.run();
+
+                output[0] = adder0.output[0];
+                output[1] = adder1.output[0];
+                output[2] = adder2.output[0];
+                output[3] = adder3.output[0];
+                output[4] = adder4.output[0];
+                output[5] = adder5.output[0];
+                output[6] = adder6.output[0];
+                output[7] = adder7.output[0];
+                output[8] = adder8.output[0];
+                output[9] = adder9.output[0];
+                output[10] = adder10.output[0];
+                output[11] = adder11.output[0];
+                output[12] = adder12.output[0];
+                output[13] = adder13.output[0];
+                output[14] = adder14.output[0];
+                output[15] = adder15.output[0];
+
+                output[16] = adder15.output[1];
+            }
     };
 }
--- main.cpp
+++ main.cpp
@@ -3,29 +3,53 @@
 #include "./Chips/Chips.h"
 
 int main() {
-    Chips::Adder8Bit adder0;
+    Chips::Adder16Bit adder0;
 
-    adder0.input[0] = 1;
+    adder0.input[0] = 0;
     adder0.input[1] = 0;
     adder0.input[2] = 1;
     adder0.input[3] = 0;
     adder0.input[4] = 0;
-    adder0.input[5] = 0;
+    adder0.input[5] = 1;
     adder0.input[6] = 0;
     adder0.input[7] = 0;
-
     adder0.input[8] = 1;
     adder0.input[9] = 1;
     adder0.input[10] = 1;
-    adder0.input[11] = 1;
+    adder0.input[11] = 0;
     adder0.input[12] = 0;
     adder0.input[13] = 0;
-    adder0.input[14] = 0;
+    adder0.input[14] = 1;
     adder0.input[15] = 0;
+
+    adder0.input[16] = 0;
+    adder0.input[17] = 1;
+    adder0.input[18] = 0;
+    adder0.input[19] = 1;
+    adder0.input[20] = 1;
+    adder0.input[21] = 1;
+    adder0.input[22] = 1;
+    adder0.input[23] = 0;
+    adder0.input[24] = 1;
+    adder0.input[25] = 0;
+    adder0.input[26] = 0;
+    adder0.input[27] = 1;
+    adder0.input[28] = 1;
+    adder0.input[29] = 0;
+    adder0.input[30] = 1;
+    adder0.input[31] = 0;
 
 
     adder0.run();
 
+    std::cout << adder0.output[15] << std::endl;
+    std::cout << adder0.output[14] << std::endl;
+    std::cout << adder0.output[13] << std::endl;
+    std::cout << adder0.output[12] << std::endl;
+    std::cout << adder0.output[11] << std::endl;
+    std::cout << adder0.output[10] << std::endl;
+    std::cout << adder0.output[9] << std::endl;
+    std::cout << adder0.output[8] << std::endl;
     std::cout << adder0.output[7] << std::endl;
     std::cout << adder0.output[6] << std::endl;
     std::cout << adder0.output[5] << std::endl;
@@ -35,7 +59,7 @@ int main() {
     std::cout << adder0.output[1] << std::endl;
     std::cout << adder0.output[0] << std::endl;
 
-    std::cout << std::endl << adder0.output[8] << std::endl;
+    std::cout << std::endl << adder0.output[16] << std::endl;
 
     return 0;
 }

f9bafa539667a37161cb0e959b7483d55989b341
ManavGhaiCode <manavghai12october@gmail.com>
2023-09-04 21:48:12 +0530

    added subtration

--- Chips/Chips.h
+++ Chips/Chips.h
@@ -51,7 +51,7 @@ namespace Chips {
             Adder adder7;
         
         public:
-            void run() {
+            void run() override {
                 adder0.input[0] = input[0];
                 adder0.input[1] = input[8];
                 adder0.input[2] = 0;
@@ -114,7 +114,7 @@ namespace Chips {
 
     };
 
-    class Adder16Bit : public Gates::CustomGate<16, 9> {
+    class Adder16Bit : public Gates::CustomGate<33, 17> {
         private:
             Adder adder0;
             Adder adder1;
@@ -134,10 +134,10 @@ namespace Chips {
             Adder adder15;
         
         public:
-            void run() {
+            void run() override {
                 adder0.input[0] = input[0];
                 adder0.input[1] = input[16];
-                adder0.input[2] = 0;
+                adder0.input[2] = input[32];
 
                 adder0.run();
 
@@ -249,6 +249,150 @@ namespace Chips {
                 output[15] = adder15.output[0];
 
                 output[16] = adder15.output[1];
+            }
+    };
+
+    class SALU : public Gates::CustomGate<33, 18> {
+        private:
+            Gates::XORGate xor0;
+            Gates::XORGate xor1;
+            Gates::XORGate xor2;
+            Gates::XORGate xor3;
+            Gates::XORGate xor4;
+            Gates::XORGate xor5;
+            Gates::XORGate xor6;
+            Gates::XORGate xor7;
+            Gates::XORGate xor8;
+            Gates::XORGate xor9;
+            Gates::XORGate xor10;
+            Gates::XORGate xor11;
+            Gates::XORGate xor12;
+            Gates::XORGate xor13;
+            Gates::XORGate xor14;
+            Gates::XORGate xor15;
+
+            Adder16Bit adder16_bit0;
+
+            Gates::andGate and0;
+            Gates::notGate not0;
+
+        public:
+            void run() override {
+                xor0.input[0] = input[16];
+                xor1.input[0] = input[17];
+                xor2.input[0] = input[18];
+                xor3.input[0] = input[19];
+                xor4.input[0] = input[20];
+                xor5.input[0] = input[21];
+                xor6.input[0] = input[22];
+                xor7.input[0] = input[23];
+                xor8.input[0] = input[24];
+                xor9.input[0] = input[25];
+                xor10.input[0] = input[26];
+                xor11.input[0] = input[27];
+                xor12.input[0] = input[28];
+                xor13.input[0] = input[29];
+                xor14.input[0] = input[30];
+                xor15.input[0] = input[31];
+
+                xor0.input[1] = input[32];
+                xor1.input[1] = input[32];
+                xor2.input[1] = input[32];
+                xor3.input[1] = input[32];
+                xor4.input[1] = input[32];
+                xor5.input[1] = input[32];
+                xor6.input[1] = input[32];
+                xor7.input[1] = input[32];
+                xor8.input[1] = input[32];
+                xor9.input[1] = input[32];
+                xor10.input[1] = input[32];
+                xor11.input[1] = input[32];
+                xor12.input[1] = input[32];
+                xor13.input[1] = input[32];
+                xor14.input[1] = input[32];
+                xor15.input[1] = input[32];
+
+                xor0.run();
+                xor1.run();
+                xor2.run();
+                xor3.run();
+                xor4.run();
+                xor5.run();
+                xor6.run();
+                xor7.run();
+                xor8.run();
+                xor9.run();
+                xor10.run();
+                xor11.run();
+                xor12.run();
+                xor13.run();
+                xor14.run();
+                xor15.run();
+
+                adder16_bit0.input[0] = input[0];
+                adder16_bit0.input[1] = input[1];
+                adder16_bit0.input[2] = input[2];
+                adder16_bit0.input[3] = input[3];
+                adder16_bit0.input[4] = input[4];
+                adder16_bit0.input[5] = input[5];
+                adder16_bit0.input[6] = input[6];
+                adder16_bit0.input[7] = input[7];
+                adder16_bit0.input[8] = input[8];
+                adder16_bit0.input[9] = input[9];
+                adder16_bit0.input[10] = input[10];
+                adder16_bit0.input[11] = input[11];
+                adder16_bit0.input[12] = input[12];
+                adder16_bit0.input[13] = input[13];
+                adder16_bit0.input[14] = input[14];
+                adder16_bit0.input[15] = input[15];
+                adder16_bit0.input[16] = xor0.output;
+                adder16_bit0.input[17] = xor1.output;
+                adder16_bit0.input[18] = xor2.output;
+                adder16_bit0.input[19] = xor3.output;
+                adder16_bit0.input[20] = xor4.output;
+                adder16_bit0.input[21] = xor5.output;
+                adder16_bit0.input[22] = xor6.output;
+                adder16_bit0.input[23] = xor7.output;
+                adder16_bit0.input[24] = xor8.output;
+                adder16_bit0.input[25] = xor9.output;
+                adder16_bit0.input[26] = xor10.output;
+                adder16_bit0.input[27] = xor11.output;
+                adder16_bit0.input[28] = xor12.output;
+                adder16_bit0.input[29] = xor13.output;
+                adder16_bit0.input[30] = xor14.output;
+                adder16_bit0.input[31] = xor15.output;
+
+                adder16_bit0.input[32] = input[32];
+                adder16_bit0.run();
+
+                output[0] = adder16_bit0.output[0];
+                output[1] = adder16_bit0.output[1];
+                output[2] = adder16_bit0.output[2];
+                output[3] = adder16_bit0.output[3];
+                output[4] = adder16_bit0.output[4];
+                output[5] = adder16_bit0.output[5];
+                output[6] = adder16_bit0.output[6];
+                output[7] = adder16_bit0.output[7];
+                output[8] = adder16_bit0.output[8];
+                output[9] = adder16_bit0.output[9];
+                output[10] = adder16_bit0.output[10];
+                output[11] = adder16_bit0.output[11];
+                output[12] = adder16_bit0.output[12];
+                output[13] = adder16_bit0.output[13];
+                output[14] = adder16_bit0.output[14];
+                output[15] = adder16_bit0.output[15];
+
+                output[16] = adder16_bit0.output[15];
+
+                not0.input = input[32];
+                not0.run();
+
+                and0.input[0] = adder16_bit0.output[16];
+                and0.input[1] = not0.output;
+
+                and0.run();
+
+                output[17] = and0.output;
             }
     };
 }
--- main.cpp
+++ main.cpp
@@ -3,63 +3,64 @@
 #include "./Chips/Chips.h"
 
 int main() {
-    Chips::Adder16Bit adder0;
+    Chips::SALU aslu;
 
-    adder0.input[0] = 0;
-    adder0.input[1] = 0;
-    adder0.input[2] = 1;
-    adder0.input[3] = 0;
-    adder0.input[4] = 0;
-    adder0.input[5] = 1;
-    adder0.input[6] = 0;
-    adder0.input[7] = 0;
-    adder0.input[8] = 1;
-    adder0.input[9] = 1;
-    adder0.input[10] = 1;
-    adder0.input[11] = 0;
-    adder0.input[12] = 0;
-    adder0.input[13] = 0;
-    adder0.input[14] = 1;
-    adder0.input[15] = 0;
+    aslu.input[0] = 0;
+    aslu.input[1] = 0;
+    aslu.input[2] = 1;
+    aslu.input[3] = 0;
+    aslu.input[4] = 0;
+    aslu.input[5] = 1;
+    aslu.input[6] = 0;
+    aslu.input[7] = 0;
+    aslu.input[8] = 1;
+    aslu.input[9] = 1;
+    aslu.input[10] = 1;
+    aslu.input[11] = 0;
+    aslu.input[12] = 0;
+    aslu.input[13] = 1;
+    aslu.input[14] = 1;
+    aslu.input[15] = 0;
 
-    adder0.input[16] = 0;
-    adder0.input[17] = 1;
-    adder0.input[18] = 0;
-    adder0.input[19] = 1;
-    adder0.input[20] = 1;
-    adder0.input[21] = 1;
-    adder0.input[22] = 1;
-    adder0.input[23] = 0;
-    adder0.input[24] = 1;
-    adder0.input[25] = 0;
-    adder0.input[26] = 0;
-    adder0.input[27] = 1;
-    adder0.input[28] = 1;
-    adder0.input[29] = 0;
-    adder0.input[30] = 1;
-    adder0.input[31] = 0;
+    aslu.input[16] = 0;
+    aslu.input[17] = 1;
+    aslu.input[18] = 0;
+    aslu.input[19] = 1;
+    aslu.input[20] = 1;
+    aslu.input[21] = 1;
+    aslu.input[22] = 1;
+    aslu.input[23] = 0;
+    aslu.input[24] = 1;
+    aslu.input[25] = 0;
+    aslu.input[26] = 0;
+    aslu.input[27] = 1;
+    aslu.input[28] = 1;
+    aslu.input[29] = 1;
+    aslu.input[30] = 0;
+    aslu.input[31] = 0;
 
+    aslu.input[32] = 1;
 
-    adder0.run();
+    aslu.run();
 
-    std::cout << adder0.output[15] << std::endl;
-    std::cout << adder0.output[14] << std::endl;
-    std::cout << adder0.output[13] << std::endl;
-    std::cout << adder0.output[12] << std::endl;
-    std::cout << adder0.output[11] << std::endl;
-    std::cout << adder0.output[10] << std::endl;
-    std::cout << adder0.output[9] << std::endl;
-    std::cout << adder0.output[8] << std::endl;
-    std::cout << adder0.output[7] << std::endl;
-    std::cout << adder0.output[6] << std::endl;
-    std::cout << adder0.output[5] << std::endl;
-    std::cout << adder0.output[4] << std::endl;
-    std::cout << adder0.output[3] << std::endl;
-    std::cout << adder0.output[2] << std::endl;
-    std::cout << adder0.output[1] << std::endl;
-    std::cout << adder0.output[0] << std::endl;
+    std::cout << aslu.output[15] << std::endl;
+    std::cout << aslu.output[14] << std::endl;
+    std::cout << aslu.output[13] << std::endl;
+    std::cout << aslu.output[12] << std::endl;
+    std::cout << aslu.output[11] << std::endl;
+    std::cout << aslu.output[10] << std::endl;
+    std::cout << aslu.output[9] << std::endl;
+    std::cout << aslu.output[8] << std::endl;
+    std::cout << aslu.output[7] << std::endl;
+    std::cout << aslu.output[6] << std::endl;
+    std::cout << aslu.output[5] << std::endl;
+    std::cout << aslu.output[4] << std::endl;
+    std::cout << aslu.output[3] << std::endl;
+    std::cout << aslu.output[2] << std::endl;
+    std::cout << aslu.output[1] << std::endl;
+    std::cout << aslu.output[0] << std::endl;
 
-    std::cout << std::endl << adder0.output[16] << std::endl;
+    std::cout << std::endl << aslu.output[16] << std::endl;
 
     return 0;
 }
